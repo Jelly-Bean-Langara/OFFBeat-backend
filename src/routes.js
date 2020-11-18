cec75e9be7edb6c3110df908aaff7caff133a6d7
Leonart Gutz <leonartgutz@hotmail.com>
2020-11-18 14:45:03 -0800

    Up

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,9 @@ import multerPostConfig from './config/multerPost';
 import multerProfileConfig from './config/multerProfile';
 import CategoryController from './controllers/CategoryController';
 import CategoryPostController from './controllers/CategoryPostController';
-import ExampleController from './controllers/ExampleController';
 import MomentController from './controllers/MomentController';
 import PostController from './controllers/PostController';
+import PostCoverController from './controllers/PostCoverController';
 import PostVisibilityController from './controllers/PostVisibilityController';
 import UserController from './controllers/UserController';
 
@@ -14,9 +14,9 @@ const routes = new Router();
 const uploadPostPicture = multer(multerPostConfig);
 const uploadProfilePicture = multer(multerProfileConfig);
 
-routes.get('/', ExampleController.index);
+routes.get('/', (req, res) => { res.json({ message: 'hey' }); });
 routes.post('/create-post', PostController.store);
-routes.post('/create-moment', uploadPostPicture.array('photos', 6), MomentController.store);
+routes.post('/create-moment', uploadPostPicture.array('photos', 8), MomentController.store);
 routes.post('/create-category', CategoryController.store);
 routes.post('/create-user', UserController.store);
 routes.get('/get-all-categories', CategoryController.index);
@@ -25,5 +25,7 @@ routes.put('/update-post-visibility', PostVisibilityController.update);
 routes.get('/get-posts-by-category', PostController.index);
 routes.get('/get-category-and-posts', CategoryPostController.show);
 routes.get('/get-categories-with-posts', CategoryPostController.index);
+routes.get('/get-post-cover', PostCoverController.show);
+routes.get('/get-post-by-id', PostController.show);
 
 export default routes;
