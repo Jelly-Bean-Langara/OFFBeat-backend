b5e1f597349a85c4f704856dda99dcf229783998
Leonart Gutz <leonartgutz@hotmail.com>
2020-12-02 21:15:22 -0800

    Final Commit

--- src/controllers/UserAuthController.js
+++ src/controllers/UserAuthController.js
@@ -1,43 +1,39 @@
 import { OAuth2Client } from 'google-auth-library';
 import url from 'url';
 import UserModel from '../models/UserModel';
-import keys from '../oauth2.keys.json'
+import keys from '../oauth2.keys.json';
 
-const statusSuccess = "success";
-const statusFailed = "failed";
+const statusSuccess = 'success';
+const statusFailed = 'failed';
 
 // create an oAuth client to authorize the API call.
 const oAuth2Client = new OAuth2Client(
   keys.web.client_id,
   keys.web.client_secret,
-  keys.web.redirect_uris[0]
+  keys.web.redirect_uris[0],
 );
 
-
 class UserAuthController {
-
   /* ------- ------- -------- --------------- auth --------- ---------- -- ------------ --- -------- ---- */
   /* returns a URL to land a google authorization page */
   async auth(request, response) {
-
     const scopes = [
       'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/userinfo.email'
+      'https://www.googleapis.com/auth/userinfo.email',
     ];
 
     // create an url to get Authorization Grant Code from user
     const authorizeUrl = oAuth2Client.generateAuthUrl({
       access_type: 'offline',
       scope: scopes,
-      prompt: 'consent'
+      prompt: 'consent',
     });
 
     // open this url to go on google consent page, authorize there to get Authorization Grant Code,
     // return the Auth code to backend.
     return response.json({
-      url: authorizeUrl
-    })
-
+      url: authorizeUrl,
+    });
   }
 
   /* ------- ------- -------- -------------- oauth2callback --------- ---------- -- ------------ --- -------- ----- */
@@ -45,19 +41,19 @@ class UserAuthController {
      2. generate tokens from it
      3. create user account if does not exists  */
   async oauth2callback(request, response) {
-    console.log('hit')
+    console.log('hit');
     async function main() {
       const oAuth2Client = await getAuthenticatedClient();
 
       const refreshToken = oAuth2Client.credentials.refresh_token;
 
-      const url = "https://www.googleapis.com/userinfo/v2/me"
+      const url = 'https://www.googleapis.com/userinfo/v2/me';
 
       const resource = await oAuth2Client.request({ url });
 
       try {
-
-        let user_name, user_email, user_id, user_picture;
+        let user_name; let user_email; let user_id; let
+          user_picture;
 
         user_id = resource.data.id;
         user_name = resource.data.name;
@@ -66,14 +62,14 @@ class UserAuthController {
 
         // check if user account exists, if not create user account
         const ifUserExist = await UserModel.checkUserAccount(resource.data.id);
-        if (ifUserExist == 0) {
+        if (ifUserExist === 0) {
           // create user
           const userData = {
             id: user_id,
             name: user_name,
             email: user_email,
             picture: user_picture,
-            refresh_token: refreshToken
+            refresh_token: refreshToken,
           };
           await UserModel.createUser(userData);
           console.log('account created');
@@ -94,21 +90,17 @@ class UserAuthController {
 
         // sending data along with url directing user back to app
         response.redirect(`offbeat://Profile/?refresh_token=${refreshToken}&user_id=${user_id}&name=${user_name}&email=${user_email}&user_picture=${user_picture}`);
-
       } catch (err) {
         return response.status(500).json({ error: err });
       }
-
     }
 
     function getAuthenticatedClient() {
       return new Promise(async (resolve, reject) => {
-        
         try {
           if (request.url.indexOf('/oauth2callback') > -1) {
-
             // acquire the code from the querystring
-            const querystring = new url.URL(request.url, 'https://www.offbeat-app.ca/oauth2callback').searchParams;
+            const querystring = new url.URL(request.url, 'https://leogutz.wmdd.ca/oauth2callback').searchParams;
             const authGrantCode = querystring.get('code');
 
             //  Authentication successful!
@@ -127,13 +119,11 @@ class UserAuthController {
         } catch (error) {
           reject(error);
         }
-      })
+      });
     }
 
     main().catch(console.error);
-
   }
-
 }
 
 export default new UserAuthController();
--- src/middlewares/UserAuthMiddleware.js
+++ src/middlewares/UserAuthMiddleware.js
@@ -1,17 +1,15 @@
 import * as yup from 'yup';
-import UserModel from '../models/UserModel';
 import superagent from 'superagent';
+import UserModel from '../models/UserModel';
 
-const statusSuccess = "success";
-const statusFailed = "failed";
+const statusSuccess = 'success';
+const statusFailed = 'failed';
 
 export default async (request, response, next) => {
-
-
   const schema = yup.object().shape({
     token: yup.string().required('something missing!'),
-    user_id: yup.string().required('something missing!'),
-  })
+    userId: yup.string().required('something missing!'),
+  });
 
   // validation
   try {
@@ -23,51 +21,45 @@ export default async (request, response, next) => {
   try {
     // generate new access token from refresh token
     superagent
-      .post("https://accounts.google.com/o/oauth2/token")
+      .post('https://accounts.google.com/o/oauth2/token')
       .send({
-        "client_id": "49440493504-6ie45lmrp564knu0kufmm4pkbths0eed.apps.googleusercontent.com",
-        "client_secret": "I86RhPcq134jNSEgWJnV7lkC",
-        "refresh_token": request.body.token,
-        "grant_type": "refresh_token",
+        client_id: '49440493504-6ie45lmrp564knu0kufmm4pkbths0eed.apps.googleusercontent.com',
+        client_secret: 'I86RhPcq134jNSEgWJnV7lkC',
+        refresh_token: request.body.token,
+        grant_type: 'refresh_token',
       })
-      .then(data => {
-
-        const access_token = JSON.parse(data.text).access_token
+      .then((data) => {
+        const { access_token } = JSON.parse(data.text);
 
         // get user id (sub) from access code
         superagent.get(`https://oauth2.googleapis.com/tokeninfo?access_token=${access_token}`)
-          .then(async data => {
-            const user_id = JSON.parse(data.text).sub
-            console.log("user_id", user_id)
+          .then(async (data) => {
+            const user_id = JSON.parse(data.text).sub;
+            console.log('user_id', user_id);
             // check if user exist in database (has account)
             const isUserExist = await UserModel.checkUserAccount(user_id);
             if (isUserExist == 0) {
               // response.json("either user account does not exist, or invalid token");
-              return response.status(500).json("either user account does not exist, or invalid token");
-            } else {
-              if (user_id === request.body.user_id) {
-                console.log("user authenticated!");
-                next();
-              } else {
-                //response.json("authentication failed!");
-                console.log("authentication failed!");
-                return response.status(500).json("authentication failed!");
-              }
+              return response.status(500).json('either user account does not exist, or invalid token');
             }
-
-          }).catch(err => {
-            console.log("authentication failed, invalid grant!")
+            if (user_id === request.body.user_id) {
+              console.log('user authenticated!');
+              next();
+            } else {
+              // response.json("authentication failed!");
+              console.log('authentication failed!');
+              return response.status(500).json('authentication failed!');
+            }
+          }).catch((err) => {
+            console.log('authentication failed, invalid grant!');
             return response.status(500).json(err);
           });
-
-      }).catch(err => {
-        console.log("authentication failed, invalid grant!")
+      }).catch((err) => {
+        console.log('authentication failed, invalid grant!');
         return response.status(500).json(err);
       });
-
   } catch (error) {
-    console.log("something went wrong!")
+    console.log('something went wrong!');
     return response.status(500).json(err);
   }
-
 };
